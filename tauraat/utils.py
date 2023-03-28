d5724c07b2e26d5d216347ec18dc73b10490c2e4
smhassanerfani <serfani@email.sc.edu>
2023-03-28 11:14:31 -0400

    Added AdjustLearningRate to utils

--- tauraat/utils.py
+++ tauraat/utils.py
@@ -112,3 +112,20 @@ def loss_decay_plot(num_epochs, train_loss, val_loss, save_path=None):
     if save_path is not None:
         plt.savefig(f"{save_path}/loss_decay.png")
     plt.show()
+
+class AdjustLearningRate:
+    num_of_iterations = 0
+
+    def __init__(self, optimizer, base_lr, max_iter, power):
+        self.optimizer = optimizer
+        self.base_lr = base_lr
+        self.max_iter = max_iter
+        self.power = power
+
+    def __call__(self, current_iter):
+        lr = self.base_lr * ((1 - float(current_iter) / self.max_iter) ** self.power)
+        self.optimizer.param_groups[0]['lr'] = lr
+        if len(self.optimizer.param_groups) > 1:
+            self.optimizer.param_groups[1]['lr'] = lr * 10
+
+        return lr
