735ef73bb7e6bb0547b8601168d5becc07d6d310
smhassanerfani <serfani@email.sc.edu>
2023-03-28 11:37:30 -0400

    Modified for 230327-211707.pth

--- tauraat/test.py
+++ tauraat/test.py
@@ -3,10 +3,10 @@ import numpy as np
 from torch.utils.data import DataLoader
 import torch.backends.cudnn as cudnn
 from dataloader import HYDRoSWOT, ToTensor
-from utils import scatter_plot
+from utils import scatter_plot, loss_decay_plot
 from model import MLP
 
-def main(mode='test'):
+def main(mode='test', plot_loss_decay=True):
 
     cudnn.enabled = True
     cudnn.benchmark = True
@@ -17,19 +17,23 @@ def main(mode='test'):
 
     elif mode == 'val':
         dataset = HYDRoSWOT(split='val', transform=ToTensor())
-        dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=2, pin_memory=True, drop_last=False)
+        dataloader = DataLoader(dataset, batch_size=128, shuffle=False, num_workers=2, pin_memory=True, drop_last=False)
 
     else:
         dataset = HYDRoSWOT(split='test', transform=ToTensor())
-        dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=2, pin_memory=True, drop_last=False)
+        dataloader = DataLoader(dataset, batch_size=128, shuffle=False, num_workers=2, pin_memory=True, drop_last=False)
 
 
     model = MLP(input_ftrs=4)
     model = model.cuda()
 
-    FILE = f'data/ml_weights/PyTorch/230317-160403.pth'
+    # FILE = f'data/ml_weights/PyTorch/230317-160403.pth'
+    FILE = f'data/ml_weights/PyTorch/230327-211707.pth'
     checkpoint = torch.load(FILE)
     model.load_state_dict(checkpoint['model_state'])
+
+    if plot_loss_decay:
+        loss_decay_plot(checkpoint['epoch'], checkpoint['train_loss'], checkpoint['val_loss'],  save_path='./data/')
 
     y_gt_lst = []
     y_pr_lst = []
@@ -59,7 +63,8 @@ def main(mode='test'):
     y_pr = dataset.y_scaler.inverse_transform(y_pr)
     y_pr = 10 ** y_pr
 
-    scatter_plot(y_gt, y_pr)
+    np.savetxt('./data/ml-test.out', np.hstack((y_gt, y_pr)), delimiter=',')
+    # scatter_plot(y_gt, y_pr)
 
 if __name__ == "__main__":
     main()
